2868c8d19f71e43df9205f8e3ea5a98d96dcef4e
苏相相 <suxiangxiang@szkingdom.com>
2022-12-10 18:01:27 +0800

    init app

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@media (min-width: 900px) {
+  $color: #ff0000;
+  h1 {
+    color: $color;
+  }
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+@media (max-width: 900px) {
+  $color: orange;
+  h1 {
+    color: $color;
+  }
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import TheWelcome from '../components/TheWelcome.vue'
+const welRef = ref(null);
+onMounted(() => {
+  console.log(welRef);
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome ref="welRef" />
+  </main>
+</template>
